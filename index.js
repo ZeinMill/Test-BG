e80bc7d4984a79882e3ba152fcb27486c8d6fef5
DezEdaz <zagagamer15@gmail.com>
2025-02-08 22:23:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎖️ กำลังจัดงานกีฬาสี 🎖️"];
+const statusMessages = ["🎖️ กำลังจัดงานกีฬาสี 🎖️", "🎵 นักร้องจำเป็น 🎵"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
